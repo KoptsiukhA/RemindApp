2933c45c246d53f4ea69e15a03c810ec96103201
Artsiom Koptsiukh <artsiom.koptsiukh@gmail.com>
2016-10-16 21:13:41 +0300

    Implemented NavigationView and Tab navigation with test fragments

--- app/src/main/java/bel/kaistra/remindapp/MainActivity.java
+++ app/src/main/java/bel/kaistra/remindapp/MainActivity.java
@@ -1,8 +1,10 @@
 package bel.kaistra.remindapp;
 
+import android.support.design.widget.NavigationView;
 import android.support.design.widget.TabLayout;
 import android.support.v4.view.ViewPager;
 import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
@@ -27,7 +29,6 @@ public class MainActivity extends AppCompatActivity {
         initToolbar();
         initTabs();
         initNavigationView();
-
 
 
     }
@@ -56,5 +57,27 @@ public class MainActivity extends AppCompatActivity {
 
     private void initNavigationView() {
         drawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar,
+                R.string.navigation_open, R.string.navigation_close);
+        drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.navigation);
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(MenuItem item) {
+                drawerLayout.closeDrawers();
+                switch (item.getItemId()) {
+                    case R.id.actionNotificationItem:
+                        showNotificationTab();
+
+                }
+                return true;
+            }
+        });
+    }
+
+    private void showNotificationTab() {
+        viewPager.setCurrentItem(Constants.TAB_TWO);
     }
 }
